293a80afde42b6e7621a131eb2949fd9077cbb46
robenavente <robenavente@gmail.com>
2025-10-26 22:53:55 -0300

    Improved readme. First skeleton

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
-# FedoraKDE-postinstall
+## FedoraKDE-postinstall
 Personal post-install automation for Fedora KDE
+
+Run script after a fresh install:
+
+
+```console
+bash <(curl -fsSL "https://github.com/robenavente/FedoraKDE-postinstall/raw/refs/heads/main/postinstall.sh")
+
+```
+
+or
+
+```console
+bash <(curl -fsSL "https://drive.usercontent.google.com/download?id=14BRYGoKRNqALwh_8W8iWbpts46__uDKP&export=download...uid=3a7bf3f4-d152-4b08-a499-ff1454cc146b&at=AKSUxGN-pOJyGXnbOMT3zKGsyH1v%3A1761493183188&confirm=t")
+```
--- postinstall.sh
+++ postinstall.sh
@@ -1,7 +1,12 @@
 #!/usr/bin/env bash
 set -e
 
+# always read from terminal
+exec </dev/tty
+
 MARKER=/var/lib/postinstall_stage1_done
+POST_INSTALL_DIR="$HOME/postinstall"
+REPO_DIR="$POST_INSTALL_DIR/FedoraKDE-postinstall"
 
 # ---------- Stage 1 ----------
 if [[ ! -f "$MARKER" ]]; then
@@ -24,10 +29,26 @@ if [[ ! -f "$MARKER" ]]; then
 fi
 
 # ---------- Stage 2 ----------
-echo ">>> Stage 2: continuing configuration after reboot..."
-# (put the rest of your setup here)
-# e.g. install packages, themes, drivers, etc.
 
+echo ">>> Stage 2: continuing configuration after reboot..."
+echo "Installing essential packages"
+
+mkdir -p "$POST_INSTALL_DIR"
+cd "$POST_INSTALL_DIR"
+
+sudo dnf install neovim htop git tmux 
+if [[ ! -d "$REPO_DIR/.git" ]]; then
+  echo ">>> Cloning postinstall repo..."
+  git clone https://github.com/robenavente/FedoraKDE-postinstall.git "$REPO_DIR"
+else
+  echo ">>> Repository already exists, pulling latest changes..."
+  (cd "$REPO_DIR" && git pull --ff-only)
+fi
+# ---------- Stage 3 ----------
+
+read -p "Would you like to configure and theme the workspace? [y/N] " REPLY
+if [[ "$REPLY" =~ ^[Yy]$ ]]; then
+  echo "cool"
+fi
 sudo rm -f "$MARKER"
 echo ">>> Post-install completed successfully."
-
