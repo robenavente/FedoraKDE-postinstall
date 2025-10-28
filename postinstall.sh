e5af7a61ef0e5b72ef98f18d723116794ee6a8cb
robenavente <robenavente@gmail.com>
2025-10-27 22:14:59 -0300

    Added themes and KDE config files

--- postinstall.sh
+++ postinstall.sh
@@ -11,7 +11,7 @@ REPO_DIR="$POST_INSTALL_DIR/FedoraKDE-postinstall"
 # ---------- Stage 1 ----------
 if [[ ! -f "$MARKER" ]]; then
   echo ">>> Performing initial system upgrade..."
-  sudo dnf --refresh upgrade -y
+  sudo dnf --refresh upgrade 
   echo ">>> System is now up to date."
 
   # Create marker so we know the first stage is finished
@@ -36,7 +36,9 @@ echo "Installing essential packages"
 mkdir -p "$POST_INSTALL_DIR"
 cd "$POST_INSTALL_DIR"
 
-sudo dnf install neovim htop git tmux 
+sudo dnf install neovim htop git tmux kvantum
+
+
 if [[ ! -d "$REPO_DIR/.git" ]]; then
   echo ">>> Cloning postinstall repo..."
   git clone https://github.com/robenavente/FedoraKDE-postinstall.git "$REPO_DIR"
@@ -48,7 +50,120 @@ fi
 
 read -p "Would you like to configure and theme the workspace? [y/N] " REPLY
 if [[ "$REPLY" =~ ^[Yy]$ ]]; then
-  echo "cool"
+    #Install Orchis theme
+    if [[ ! -d "Orchis-kde/.git" ]]; then
+      git clone https://github.com/vinceliuice/Orchis-kde.git
+    fi
+    cd "Orchis-kde"
+    sudo ./install.sh
+    cd ".."
+
+    #Install Qogir theme
+    if [[ ! -d "Qogir-kde/.git" ]]; then
+      git clone https://github.com/vinceliuice/Qogir-kde.git
+    fi
+    cd "Qogir-kde"
+    sudo ./install.sh
+    cd ".."
+    
+    #Install Colloid Theme
+    if [[ ! -d "Colloid-kde/.git" ]]; then
+      git clone https://github.com/vinceliuice/Colloid-kde.git
+    fi
+    cd "Colloid-kde"
+    sudo ./install.sh
+    cd ".."
+
+    #Install catppuccin Theme
+    if [[ ! -d "kde/.git" ]]; then
+      git clone https://github.com/catppuccin/kde.git
+    fi
+    cd "kde"
+    ./install.sh
+    cd ".."
+    
+    #Install Colloid icon theme
+    if [[ ! -d "Colloid-icon-theme/.git" ]]; then
+        git clone https://github.com/vinceliuice/Colloid-icon-theme.git
+    fi
+    cd "Colloid-icon-theme"
+    sudo ./install.sh -s all -t all
+    cd ".."
+
+    #Install Qogir icon theme
+    if [[ ! -d "Qogir-icon-theme/.git" ]]; then
+        git clone https://github.com/vinceliuice/Qogir-icon-theme.git
+    fi
+    cd "Qogir-icon-theme"
+    sudo ./install.sh 
+    cd ".."
+  
+    #Install Tela circle icon theme
+    if [[ ! -d "Tela-circle-icon-theme/.git" ]]; then
+        git clone https://github.com/vinceliuice/Tela-circle-icon-theme.git
+    fi
+    cd "Tela-circle-icon-theme"
+    sudo ./install.sh -a
+    cd ".."
+
+    #Install Nordzy icon theme
+    if [[ ! -d "Nordzy-icon/.git" ]]; then
+        git clone https://github.com/MolassesLover/Nordzy-icon.git
+    fi
+    cd "Nordzy-icon"
+    sudo ./install.sh
+    cd ".."
+
+    #Install WhiteSur cursor theme
+    if [[ ! -d "WhiteSur-cursors/.git" ]]; then
+        git clone https://github.com/vinceliuice/WhiteSur-cursors.git
+    fi
+    cd "WhiteSur-cursors"
+    sudo ./install.sh 
+    cd ".."
+
+    #Install Graphite cursor theme
+    if [[ ! -d "Graphite-cursors/.git" ]]; then
+        git clone https://github.com/vinceliuice/Graphite-cursors.git
+    fi
+    cd "Graphite-cursors"
+    sudo ./install.sh 
+    cd ".."
+
+    #Install Vimix cursor theme
+    if [[ ! -d "Vimix-cursors/.git" ]]; then
+        git clone https://github.com/vinceliuice/Vimix-cursors.git
+    fi
+    cd "Vimix-cursors"
+    sudo ./install.sh 
+    cd ".."
+
+    #Install Vimix cursor theme
+    if [[ ! -d "WhiteSur-cursors/.git" ]]; then
+        git clone https://github.com/vinceliuice/WhiteSur-cursors.git
+    fi
+    cd "WhiteSur-cursors"
+    sudo ./install.sh 
+    cd ".."
+
+
+    ##################################
+    #Applying theme and configuration
+    ##################################
+    
+    #Getting KARA plasmoid:
+    if [[ ! -d "WhiteSur-cursors/.git" ]]; then
+        git clone https://github.com/dhruv8sh/kara.git
+    fi
+    cp -r "kara"  ~/.local/share/plasma/plasmoids/org.dhruv8sh.kara
+    
+    
+
+
+
+
+    
+
 fi
 sudo rm -f "$MARKER"
 echo ">>> Post-install completed successfully."
